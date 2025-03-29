2edeaf680b353aa9aff1997d2509145717b2f69e
Sean Martinez <185059685+Sean-Pers0nal@users.noreply.github.com>
2025-03-29 19:39:38 +0800

    Delete DWEB directory

--- DWEB/includes/product_functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'config.php';
-
-function getProducts() {
-    global $conn;
-    $result = $conn->query("SELECT * FROM products");
-    return $result->fetch_all(MYSQLI_ASSOC);
-}
-?>
